8001fb1372d5f09e8377dbd295c1471102ea5955
Vayango <vayango@hotmail.fr>
2020-09-17 15:48:58 +0200

    Updates

---
+++ precache-manifest.2e2c5295b7f975ba4f91927fe4b03462.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2e62ee50691ec7debafcd3bf4fac99a0",
+    "url": "/luxuryCar/index.html"
+  },
+  {
+    "revision": "be25f32993deb0bb5b9d",
+    "url": "/luxuryCar/static/css/main.2bd81344.chunk.css"
+  },
+  {
+    "revision": "ac734963c8fcd0e3210b",
+    "url": "/luxuryCar/static/js/2.3516c28a.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/luxuryCar/static/js/2.3516c28a.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "be25f32993deb0bb5b9d",
+    "url": "/luxuryCar/static/js/main.cb93d5c9.chunk.js"
+  },
+  {
+    "revision": "42b2d5a27db86d475440",
+    "url": "/luxuryCar/static/js/runtime-main.ac49ef23.js"
+  }
+]);
